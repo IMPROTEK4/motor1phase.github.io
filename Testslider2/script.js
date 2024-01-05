b1a3ba93aa4b0af19861fbd24b909b023d2a4093
IMPROTEK4 <153419992+IMPROTEK4@users.noreply.github.com>
2024-01-05 15:51:56 +0700

    Delete Testslider2 directory

--- Testslider2/script.js
+++
@@ -1,9 +0,0 @@
-const slider = document.getElementById('mySlider');
-const sliderValue = document.getElementById('sliderValue');
-
-slider.addEventListener('input', updateSliderValue);
-
-function updateSliderValue() {
-  const value = slider.value;
-  sliderValue.textContent = value;
-}
